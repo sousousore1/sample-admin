e44ac4dace3ae1212265d0d4b661d962fe4c20fa
Soichi Saito <soichi.saito@bizreach.co.jp>
2018-12-28 15:50:12 +0900

    Fix root path

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_28_060659) do
+ActiveRecord::Schema.define(version: 2018_12_28_060241) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
